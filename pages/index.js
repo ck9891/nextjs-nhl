897ef8a7dc70e8aa27a2e5ebada01a4e394131d6
Cam Farquharson <fuller.igames@gmail.com>
2019-05-10 15:04:32 -0400

    Added new files

---
+++ pages/index.js
@@ -0,0 +1,32 @@
+import Layout from '../components/MyLayout.js'
+import Link from 'next/link'
+import fetch from 'isomorphic-unfetch'
+
+const Index = (props) => (
+  <Layout>
+    <h1>NHL Teams</h1>
+    <ul>
+      {props.teams.map((team) => (
+        <li key={team.id}>
+          <Link as={`/p/${team.id}`} href={`/post?id=${team.id}`}>
+            <a>{team.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </Layout>
+)
+
+Index.getInitialProps = async function () {
+  const resNHL = await fetch('https://statsapi.web.nhl.com/api/v1/teams/')
+  const nhlData = await resNHL.json();
+  console.log(nhlData.teams);
+
+  console.log(`Show data fetched. Count: ${nhlData.teams.length}`)
+  const nhlTeams = nhlData.teams;
+  return {
+    teams: nhlTeams
+  }
+}
+
+export default Index
---
+++ pages/post.js
@@ -0,0 +1,31 @@
+import Layout from '../components/MyLayout.js'
+import fetch from 'isomorphic-unfetch'
+
+const Post = props => (
+  <Layout>
+    <h1>{props.nhlTeam[0].name}</h1>
+    <ul>
+      {props.teamRoster.map((player) => (
+        <li key={player.person.id}>
+          {/* <Link as={`/p/${player.id}`} href={`/post?id=${player.id}`}> */}
+            <span>{player.person.fullName}</span>
+          {/* </Link> */}
+        </li>
+      ))}
+    </ul>
+  </Layout>
+)
+
+Post.getInitialProps = async function (context) {
+  const { id } = context.query
+  const res = await fetch(`https://statsapi.web.nhl.com/api/v1/teams/${id}`)
+  const team = await res.json()
+  const resTeam = await fetch(`https://statsapi.web.nhl.com/api/v1/teams/${id}/roster`);
+  const roster = await resTeam.json();
+  console.log(roster)
+  const teamRoster = roster.roster;
+  const nhlTeam = team.teams;
+  return { nhlTeam, teamRoster }
+}
+
+export default Post
